c1a4f5d1a57ddf602d92b487e07e4320a836f853
dcr9110 <diegocn217@gmail.com>
2020-01-17 20:58:28 -0300

    *Se personalizo la vista de dia

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
+  //dia
+  maniana = ["Media Noche", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  tarde = ["Medio Dia", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  
   constructor() { }
 
   ngOnInit() {
